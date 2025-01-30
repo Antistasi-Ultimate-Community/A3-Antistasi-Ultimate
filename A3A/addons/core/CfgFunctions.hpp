8c282e7c1c5a93b73655601738e261e54a894a2b
MaxxLite <162753200+UAC-MaxxLite@users.noreply.github.com>
2025-01-30 16:55:13 +0200

    recommended fixes

--- A3A/addons/core/CfgFunctions.hpp
+++ A3A/addons/core/CfgFunctions.hpp
@@ -426,7 +426,6 @@ class CfgFunctions
             class LOG_Bank {};
             class SUPP_Supplies {};
             class LOG_Salvage {};
-            class findIfNearAndHostileZones {};
             class findIfNearAndHostile {};
             class LOG_Airdrop {};
             class LOG_Crashsite {};
