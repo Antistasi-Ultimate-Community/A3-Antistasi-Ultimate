f98ab74a37d3054aa14f53bf2de005c4b5714fc4
wersal454 <wroma@bk.ru>
2025-04-12 17:39:28 +0300

    Update Params.hpp

--- A3A/addons/core/Params.hpp
+++ A3A/addons/core/Params.hpp
@@ -847,13 +847,13 @@ class Params
         texts[] = {"∞", "16", "24", "32"};
         default = 24;
     };
-    class A3A_UAVSpawnChance: BalanceParams
+    class A3A_rebelGarrisonGroupSize: BalanceParams
     {
-        title = $STR_params_UAVSpawnChance;
-        tooltip = $STR_params_UAVSpawnChance_desc;
-        values[] = {0, 0.1, 0.2, 0.3, 0.5, 1, 2};
-        texts[] = {"0", "10%", "20%", "30%", "50%", "100%", "200%"};
-        default = 0.2;
+        title = $STR_params_rebelGarrisonLimit;
+        tooltip = $STR_params_rebelGarrisonLimit_desc;
+        values[] = {2, 4, 6, 8, 10, 12, 14, 16};
+        texts[] = {"2", "4", "6", "8", "10", "12", "14", "16"};
+        default = 8;
     };
     
     class EquipmentParams: AllParams
