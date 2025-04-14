a4375065c6dc20d152e6159039880141e725e30f
stutpip123 <77023662+stutpip123@users.noreply.github.com>
2025-04-14 14:28:53 +0100

    USA

--- A3A/addons/core/Templates/Templates/ERA/templates.hpp
+++ A3A/addons/core/Templates/Templates/ERA/templates.hpp
@@ -1,15 +1,25 @@
     class ERA_Base
     {
         basepath = QPATHTOFOLDER(Templates\Templates\ERA);
-        priority = 70;
+        priority = 5;
     };
 
     class ERA_GER : ERA_Base
     {
 		requiredAddons[] = {"ww2_spe_assets_c_characters_germans_c","bwa3_common","gm_weapons_items","CUP_Creatures_People_Civil_Russia", "CUP_BaseConfigs", "CUP_AirVehicles_Core"};
-        side = "Occ";
+        side = "Inv";
         flagTexture = "bwa3_common\data\bwa3_flag_germany_co.paa";
 		logo = "bwa3_common\data\bwa3_flag_germany_co.paa";
         name = "ERA Germany";
         file = "ERA_AI_GER";
+    };
+	
+	class ERA_USA : ERA_Base
+    {
+		requiredAddons[] = {"ww2_spe_assets_c_characters_germans_c","vn_weapons","CUP_Creatures_People_Civil_Russia", "CUP_BaseConfigs", "CUP_AirVehicles_Core"};
+        side = "Occ";
+        flagTexture = "a3\data_f\flags\flag_us_co.paa";
+		logo = "a3\data_f\flags\flag_us_co.paa";
+        name = "ERA USA";
+        file = "ERA_AI_USA";
     };
