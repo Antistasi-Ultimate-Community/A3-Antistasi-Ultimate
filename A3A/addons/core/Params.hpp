3d54bf6b613db67395977028ca7d7b61844aa0fa
jwoodruff40 <22091381+jwoodruff40@users.noreply.github.com>
2025-10-23 19:47:11 -0400

    Param to limit how often incap / uncon is triggered

--- A3A/addons/core/Params.hpp
+++ A3A/addons/core/Params.hpp
@@ -596,12 +596,19 @@ class Params
         texts[] = {"5", "10", "15", "20"};
         default = 5;
     };
-    class limitWeaponsByUnitType : ExperimentalParams
+    class unconChanceEny : ExperimentalParams
     {
-        title = $STR_params_limitWeaponsByUnitType;
-        values[] = {0, 1};
-        texts[] = {$STR_antistasi_dialogs_generic_button_no_text, $STR_antistasi_dialogs_generic_button_yes_text};
-        default = 1;
+        title = $STR_params_unconChanceEny;
+        values[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        texts[] = {"0%", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"};
+        default = 10;
+    };
+    class unconChanceReb : ExperimentalParams
+    {
+        title = $STR_params_unconChanceReb;
+        values[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        texts[] = {"0%", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"};
+        default = 10;
     };
 
     class BalanceParams
