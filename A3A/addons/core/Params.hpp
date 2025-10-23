a4fb0f777796e19497408e260db49ebf78f9c46f
jwoodruff40 <22091381+jwoodruff40@users.noreply.github.com>
2025-10-23 19:47:11 -0400

    Param to limit how often incap / uncon is triggered

--- A3A/addons/core/Params.hpp
+++ A3A/addons/core/Params.hpp
@@ -724,6 +724,20 @@ class Params
         texts[] = {"5", "10", "15", "20"};
         default = 5;
     };
+    class unconChanceEny : ExperimentalParams
+    {
+        title = $STR_params_unconChanceEny;
+        values[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        texts[] = {"0%", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"};
+        default = 10;
+    };
+    class unconChanceReb : ExperimentalParams
+    {
+        title = $STR_params_unconChanceReb;
+        values[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        texts[] = {"0%", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"};
+        default = 10;
+    };
 
     class BalanceParams
     {
