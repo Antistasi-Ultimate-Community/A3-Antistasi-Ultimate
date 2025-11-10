5f9d1ed01ae1ec145e704d860d5076974351110f
wersal454 <wroma@bk.ru>
2025-11-11 00:25:00 +0300

    quick fix

--- A3A/addons/ultimate/patches/cfgVehicles.hpp
+++ A3A/addons/ultimate/patches/cfgVehicles.hpp
@@ -191,6 +191,22 @@ class CfgVehicles {
         };
     };
 
+    class UserTexture10m_F;
+    class OverObject_HelipadCircle_F: UserTexture10m_F {
+        /* hiddenSelections[] = {"camo", "decals"};
+        hiddenSelectionsTextures[] = {
+            "\A3\Structures_F\Mil\Helipads\Data\HelipadCircle_F_CO.paa",
+            "\A3\Structures_F\Mil\Helipads\Data\HelipadDecal_F_CO.paa"
+        }; */
+    };
+    class OverObject_HelipadSquare_F: UserTexture10m_F {
+        /* hiddenSelections[] = {"camo", "decals"};
+        hiddenSelectionsTextures[] = {
+            "\A3\Structures_F\Mil\Helipads\Data\HelipadSquare_F_CO.paa",
+            "\A3\Structures_F\Mil\Helipads\Data\HelipadDecal_F_CO.paa"
+        }; */
+    };
+
     class Land_ButaneTorch_F;
 
     // BB -> base builder
@@ -281,24 +297,4 @@ class Extended_InitPost_EventHandlers {
             clientInit = QUOTE(call A3A_fnc_handlerTerrainObjectHiderPostInitClient);
         };
     };
-};
-class Land_HelipadCircle_F;
-class Land_HelipadSquare_F;
-class A3AU_RebHelipad_Circle_F: Land_HelipadCircle_F {};
-class A3AU_RebHelipad_Square_F: Land_HelipadSquare_F {};
-
-class UserTexture10m_F;
-class OverObject_HelipadCircle_F: UserTexture10m_F {
-    /* hiddenSelections[] = {"camo", "decals"};
-    hiddenSelectionsTextures[] = {
-        "\A3\Structures_F\Mil\Helipads\Data\HelipadCircle_F_CO.paa",
-        "\A3\Structures_F\Mil\Helipads\Data\HelipadDecal_F_CO.paa"
-    }; */
-};
-class OverObject_HelipadSquare_F: UserTexture10m_F {
-    /* hiddenSelections[] = {"camo", "decals"};
-    hiddenSelectionsTextures[] = {
-        "\A3\Structures_F\Mil\Helipads\Data\HelipadSquare_F_CO.paa",
-        "\A3\Structures_F\Mil\Helipads\Data\HelipadDecal_F_CO.paa"
-    }; */
 };
