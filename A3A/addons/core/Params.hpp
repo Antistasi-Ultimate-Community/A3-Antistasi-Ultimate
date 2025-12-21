241b0d46e7b0fc8e1d64d28d9303090c03e30a9b
jwoodruff40 <22091381+jwoodruff40@users.noreply.github.com>
2025-12-21 16:28:35 -0500

    change default for limiting weapons by AI class to no

--- A3A/addons/core/Params.hpp
+++ A3A/addons/core/Params.hpp
@@ -1183,7 +1183,7 @@ class Params
         title = $STR_params_limitWeaponsByUnitType;
         values[] = {0, 1};
         texts[] = {$STR_antistasi_dialogs_generic_button_no_text, $STR_antistasi_dialogs_generic_button_yes_text};
-        default = 1;
+        default = 0;
     };
     class A3U_HelipadTerrainSmoothing: ExperimentalParams
     {
