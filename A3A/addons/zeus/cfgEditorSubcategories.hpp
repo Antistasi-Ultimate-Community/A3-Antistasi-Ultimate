be3ab16edbf6892241ba5fcedcba953871d523f0
jwoodruff40 <22091381+jwoodruff40@users.noreply.github.com>
2026-01-18 16:07:33 -0500

    formatting

--- A3A/addons/zeus/cfgEditorSubcategories.hpp
+++ A3A/addons/zeus/cfgEditorSubcategories.hpp
@@ -1,27 +1,21 @@
 class CfgEditorSubcategories
 {
-	class GVAR(Militia)
-	{
+	class GVAR(Militia) {
 		displayName = $STR_A3U_Zeus_Category_Men_Militia;
 	};
-	class GVAR(Military)
-	{
+	class GVAR(Military) {
 		displayName = $STR_A3U_Zeus_Category_Men_Military;
 	};
-	class GVAR(Elite)
-	{
+	class GVAR(Elite) {
 		displayName = $STR_A3U_Zeus_Category_Men_Elite;
 	};
-	class GVAR(SpecialForces)
-	{
+	class GVAR(SpecialForces) {
 		displayName = $STR_A3U_Zeus_Category_Men_SpecialForces;
 	};
-	class GVAR(Police)
-	{
+	class GVAR(Police) {
 		displayName = $STR_A3U_Zeus_Category_Men_Police;
 	};
-	class GVAR(Other)
-	{
+	class GVAR(Other) {
 		displayName = $STR_A3U_Zeus_Category_Men_Other;
 	};
 };
--- A3A/addons/zeus/cfgFactionClasses.hpp
+++ A3A/addons/zeus/cfgFactionClasses.hpp
@@ -1,39 +1,35 @@
 class CfgFactionClasses
 {
-	class GVAR(Rebels)
-	{
+	// factions
+	class GVAR(Rebels) {
 		displayName = $STR_A3U_Zeus_Faction_Rebels;
 		side = 2;
 		//flag = "";
 		//icon = "\A3A\addons\ultimate\data\rebellion_icon.paa";
 		priority = 1;
 	};
-	class GVAR(Occupants)
-	{
+	class GVAR(Occupants) {
 		displayName = $STR_A3U_Zeus_Faction_Occupants;
 		side = 1;
 		//flag = "";
 		//icon = "\A3A\addons\ultimate\data\civ_icon.paa";
 		priority = 1;
 	};
-	class GVAR(Invaders)
-	{
+	class GVAR(Invaders) {
 		displayName = $STR_A3U_Zeus_Faction_Invaders;
 		side = 0;
 		//flag = "";
 		//icon = "\A3A\addons\ultimate\data\invaders_icon.paa";
 		priority = 1;
 	};
-	class GVAR(Rivals)
-	{
+	class GVAR(Rivals) {
 		displayName = $STR_A3U_Zeus_Faction_Rivals;
 		side = 0;
 		//flag = "";
 		//icon = "\A3A\addons\ultimate\data\rivals_icon.paa";
 		priority = 1;
 	};
-	class GVAR(Civilians)
-	{
+	class GVAR(Civilians) {
 		displayName = $STR_A3U_Zeus_Faction_Civilians;
 		side = 3;
 		//flag = "";
