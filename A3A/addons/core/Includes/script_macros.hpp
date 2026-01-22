be878d4c93f9c01d20f7e8111b46ad3faf2ec789
Oliver Schieche <post@oliver-schieche.de>
2026-01-22 10:16:11 +0100

    Time to move those to their own .hpp

--- A3A/addons/core/Includes/script_macros.hpp
+++ A3A/addons/core/Includes/script_macros.hpp
@@ -1,16 +1,6 @@
 #include "\x\cba\addons\main\script_macros_common.hpp"
 //#include "script_macros_undef.hpp"
-
-// Client-only event; after client initialization; params=[]
-#define CBA_EVENT_CLIENT_INIT_DONE QUOTE(TRIPLES(PREFIX,event,clientInitDone))
-// Client-only event; on personal save loaded; params=[Hashmap saveData]
-#define CBA_EVENT_CLIENT_PLAYER_LOAD QUOTE(TRIPLES(PREFIX,event,clientPlayerLoad))
-// Client-only event; on personal save; params=[Hashmap saveData]
-#define CBA_EVENT_CLIENT_PLAYER_SAVE QUOTE(TRIPLES(PREFIX,event,clientPlayerSave))
-// Server-only event; after server initialization; params=[]
-#define CBA_EVENT_SERVER_INIT_DONE QUOTE(TRIPLES(PREFIX,event,serverInitDone))
-// Server-only event; on game save; params=[]
-#define CBA_EVENT_SERVER_GAME_SAVED QUOTE(TRIPLES(PREFIX,event,serverGameSaved))
+#include "cba_events.hpp"
 
 #define PATCHNAME(x) TRIPLES(PREFIX,COMPONENT,x)
 // CBA uses "fnc", we use "fn" to look for function source files ...
