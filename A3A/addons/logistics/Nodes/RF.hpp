2a57a40dd8a0192178066f900e7d63f5f2de8825
wersal <wroma@bk.ru>
2026-01-25 13:46:16 +0300

    Update RF.hpp
    Signed-off-by: wersal <wroma@bk.ru>

--- A3A/addons/logistics/Nodes/RF.hpp
+++ A3A/addons/logistics/Nodes/RF.hpp
@@ -114,3 +114,20 @@ class lxRF_vehicles_rf_pickup_01_pickup_01_service_rf_p3d : TRIPLES(ADDON,Nodes,
         };
     };
 };
+
+class lxRF_vehicles_rf_pickup_01_pickup_01_comms_rf_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-1.5,-1};
+            seats[] = {5,6};
+        };
+        class Node2
+        {
+            offset[] = {0,-2.3,-1};
+            seats[] = {3,4};
+        };
+    };
+};
