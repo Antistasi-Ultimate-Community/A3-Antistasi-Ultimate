d549c3486d83f03a816cf5b8e1f834928fe02a5d
wersal454 <wroma@bk.ru>
2026-01-28 13:40:37 +0300

    Gyra adjustments

--- A3A/addons/logistics/Cargo/EF.hpp
+++ A3A/addons/logistics/Cargo/EF.hpp
@@ -6,9 +6,11 @@ class ef_ef_static_ef_lpd_turret_1_p3d : TRIPLES(ADDON,Cargo,Base) /////need to 
     recoil = 250;
     isWeapon = 1;
 };
+
+//gyra apc
 class ef_ef_gyra_m_ef_gyra_armed_p3d : TRIPLES(ADDON,Cargo,Base)
 {
-    offset[] = {0,0,2.503};
+    offset[] = {-0.15,-0.1,1.646};
     rotation[] = {0,1,0};
     size = 7;
     recoil = 1000;
@@ -16,7 +18,7 @@ class ef_ef_gyra_m_ef_gyra_armed_p3d : TRIPLES(ADDON,Cargo,Base)
 };
 class ef_ef_gyra_m_ef_gyra_mortar_p3d : TRIPLES(ADDON,Cargo,Base)
 {
-    offset[] = {0,0,2.503};
+    offset[] = {-0.15,-0.1,1.62};
     rotation[] = {0,1,0};
     size = 7;
     recoil = 2500;
@@ -24,7 +26,7 @@ class ef_ef_gyra_m_ef_gyra_mortar_p3d : TRIPLES(ADDON,Cargo,Base)
 };
 class ef_ef_gyra_m_ef_gyra_hmg_p3d : TRIPLES(ADDON,Cargo,Base)
 {
-    offset[] = {0,0,2.503};
+    offset[] = {-0.15,-0.1,1.72};
     rotation[] = {0,1,0};
     size = 7;
     recoil = 150;
@@ -32,7 +34,7 @@ class ef_ef_gyra_m_ef_gyra_hmg_p3d : TRIPLES(ADDON,Cargo,Base)
 };
 class ef_ef_gyra_m_ef_gyra_antiair_p3d : TRIPLES(ADDON,Cargo,Base)
 {
-    offset[] = {0,0,2.503};
+    offset[] = {-0.15,-0.1,1.647};
     rotation[] = {0,1,0};
     size = 7;
     recoil = 1000;
@@ -40,7 +42,7 @@ class ef_ef_gyra_m_ef_gyra_antiair_p3d : TRIPLES(ADDON,Cargo,Base)
 };
 class ef_ef_gyra_m_ef_gyra_unarmed_p3d : TRIPLES(ADDON,Cargo,Base)
 {
-    offset[] = {0,0,2.503};
+    offset[] = {-0.15,-0.1,1.498};
     rotation[] = {0,1,0};
     size = 7;
 };
