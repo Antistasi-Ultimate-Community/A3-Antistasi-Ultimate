ed7bd1fab51d39d496dda6b633aaa83bbc49cb10
Oliver Schieche <post@oliver-schieche.de>
2026-01-31 18:29:43 +0100

    Added scanHorizon to AT emplacements, replaced 300 hardcode with variable

--- A3A/addons/core/functions/init/varsHardcoded.hpp
+++ A3A/addons/core/functions/init/varsHardcoded.hpp
@@ -12,6 +12,9 @@ Author:
     UnseenKill/gor3Splatter
 ---------------------------------------------------------------------------- */
 
+// Used as distance in calls to SCRT_fnc_common_scanHorizon
+GVAR(scanHorizonDistance) = 300;
+
 // When getting marker sizes for zero-size markers (emplacements), pretend
 // they're really this big.
 GVAR(zeroSizeMarkerBlowup) = 50;
