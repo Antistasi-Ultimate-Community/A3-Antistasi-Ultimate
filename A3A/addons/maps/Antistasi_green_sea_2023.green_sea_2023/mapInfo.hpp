e21f090ee26ac10c48fbb55d8b54105b4996dc85
stutpip123 <77023662+stutpip123@users.noreply.github.com>
2026-02-25 17:27:35 +0000

    Green Sea 2023 initial port

---
+++ A3A/addons/maps/Antistasi_green_sea_2023.green_sea_2023/mapInfo.hpp
@@ -0,0 +1,132 @@
+#include "..\BuildObjectsList.hpp"
+class green_sea_2023 {
+	population[] = {
+{"city_Chernogorsk", 575},
+{"city_Elektrozavodsk", 527},
+{"vill_Tulga", 35},
+{"city_Solnychniy", 182},
+{"city_Berezino", 281},
+{"city_StarySobor", 151},
+{"vill_Msta", 77},
+{"vill_Staroye", 94},
+{"vill_Shakhovka", 51},
+{"vill_Dolina", 76},
+{"vill_Orlovets", 57},
+{"vill_Bor", 53},
+{"city_Gorka", 137},
+{"vill_NovySobor", 111},
+{"vill_Kabanino", 79},
+{"vill_Mogilevka", 100},
+{"vill_Nadezhdino", 82},
+{"vill_Guglovo", 26},
+{"vill_Kamyshovo", 169},
+{"vill_Pusta", 47},
+{"city_Zelenogorsk", 447},
+{"city_Vybor", 188},
+{"vill_Dubrovka", 94},
+{"vill_Khelm", 88},
+{"vill_Olsha", 118},
+{"vill_Gvozdno", 92},
+{"city_Krasnostav", 180},
+{"vill_Grishino", 121},
+{"vill_Rogovo", 64},
+{"vill_Pogorevka", 65},
+{"vill_Pustoshka", 157},
+{"vill_Kozlovka", 95},
+{"vill_Polana", 123},
+{"vill_Balota", 133},
+{"vill_Komarovo", 112},
+{"vill_Kamenka", 110},
+{"vill_Myshkino", 96},
+{"vill_Pavlovo", 94},
+{"vill_Lopatino", 120},
+{"vill_Vyshnoye", 38},
+{"vill_Prigorodki", 127},
+{"vill_Drozhino", 90},
+{"vill_Sosnovka", 45},
+{"vill_Nizhnoye", 113},
+{"vill_Pulkovo", 36},
+{"vill_Zvir", 102},
+{"vil_Bogtyrka", 147},
+{"vil_vavilovo", 121},
+{"vil_Zabolotye", 55},
+{"Vil_Sinystok", 106},
+{"Vil_Topolniki", 232},
+{"City_NovayaPetrovka", 544},
+{"Vil_Tisy", 83},
+{"Settlement_Zaprudnoe", 141},
+{"Settlement_Ratnoe", 82},
+{"Settlement_Polesovo", 116},
+{"Settlement_Skalka", 52},
+{"Settlement_Krasnoe", 29},
+{"Settlement_StaryYar", 345},
+{"Settlement_Kamensk", 145},
+{"Settlement_Kalinovka", 133},
+{"Settlement_Arsenovo", 21},
+{"City_Severograd", 355},
+{"Settlement_Svergino", 163},
+{"Settlement_Nagornoe", 176},
+{"city_Novodmitrovsk", 576},
+{"vil_Karmanovka", 125},
+{"vil_Dobroye", 49},
+{"vil_BelayaPolyana", 92},
+{"vil_Turovo", 85},
+{"vil_Berezhki", 26},
+{"city_Svetloyarsk", 465},
+{"city_cernayapolana", 220},
+{"ind_lesozavod", 11},
+{"vil_Polyanskoye", 29},
+{"vil_Podgornoye", 31},
+{"vil_Rybachiy", 125},
+{"vil_Lesozavodsk", 146},
+{"vil_Nochnyye_Gory", 41},
+{"vil_Beregovoye", 19},
+{"vil_Rodnikovo", 75},
+{"vil_Lugovoye", 48},
+{"vil_Peschansk", 108},
+{"vil_Pepelnaya_Krepost", 325},
+{"vil_Zernitsk", 290},
+{"vil_Vetrovsk", 345},
+{"vil_Uyutnyy_Kray", 153},
+{"vil_Pribrezhye", 56},
+{"vil_Pervomayskiy", 95},
+{"vil_Divnogorsk", 157},
+{"vil_Baranovo", 138},
+{"vil_Mirnyy_Khutor", 91},
+{"vil_Glukhoye", 53},
+{"vil_Rassvetnyy", 156},
+{"vil_Sosnovoye", 114},
+{"vil_Poputniy", 81},
+{"Caribou_isl051", 0},
+{"name_Zhitnya", 139},
+{"name_Kulima", 139},
+{"name_Pustoye", 14},
+{"name_Tikhoye", 96},
+{"island_Bolshevik", 0}
+	};
+	disabledTowns[] = {"Settlement_Krasnoe", "Settlement_Arsenovo"};
+	antennas[] = {
+		{9822.91,10314.4,0},{3707.2,14751.2,0},{7175.2,3018.23,0},{1275.49,6215.75,0},{3688.6,5958.29,0},{13326.2,3256.85,0},{514.324,11082.6,0},{11445.2,7565.58,0},{13326.4,3257.08,0},{6874.77,11458.9,0},{11560.7,11313.6,0},{12936.5,12763.4,0}
+	};
+	antennasBlacklistIndex[] = {};
+	banks[] = {
+		{6831.07,2433.6,0},{12127.5,9093.7,0},{10396.5,2266.98,0}
+	};
+	garrison[] = {
+		{},{"airport_3","outpost_24","outpost_20", "outpost_23", "outpost_20","seaport_5","control_143", "control_144", "control_145", "control_149", "control_147","control_169", "control_165", "control_138", "control_137", "control_158"},{},{"control_143", "control_144", "control_145", "control_149", "control_147","control_169", "control_165", "control_138", "control_137", "control_158"}
+	};
+	fuelStationTypes[] = {
+		"Land_FuelStation_Feed_F","Land_fs_feed_F","Land_FuelStation_01_pump_malevil_F","Land_FuelStation_01_pump_F","Land_FuelStation_02_pump_F","Land_FuelStation_03_pump_F","Land_A_FuelStation_Feed","Land_Ind_FuelStation_Feed_EP1","Land_FuelStation_Feed_PMC","Land_Fuelstation","Land_Fuelstation_army","Land_Benzina_schnell"
+	};
+	milAdministrations[] = {
+		{2729.96,5375.75,0},{13851.8,13218.4,0},{11489,14480.1,0},{7735.05,12636.6,0},{3430.91,13015.8,0},{11988.3,9190.38,3.8147e-006},{6617.86,2389.05,0},{6075.08,7777.44,0},{10699,7986.4,0}
+	};
+	climate = "temperate";
+	buildObjects[] = {
+		BUILDABLES_HISTORIC,
+		BUILDABLES_MODERN_GREEN,
+		BUILDABLES_TEMPERATE,
+		BUILDABLES_CUP,
+		BUILDABLES_UNIVERSAL
+	};
+};
