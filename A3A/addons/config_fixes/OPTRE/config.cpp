749c990ea9daaf9f1257a8a55ea540093c224f6f
wersal <wroma@bk.ru>
2026-03-18 19:06:24 +0300

    Create config.cpp
    Signed-off-by: wersal <wroma@bk.ru>

---
+++ A3A/addons/config_fixes/OPTRE/config.cpp
@@ -0,0 +1,25 @@
+//OPTRE - config.cpp
+
+#include "..\script_component.hpp"
+
+class CfgPatches 
+{
+    class PATCHNAME(OPTRE) 
+    {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"OPTRE_Core", "OPTRE_FC_Core"};
+        author = AUTHOR;
+        authors[] = { AUTHORS };
+        authorUrl = "";
+        VERSION_CONFIG;
+        skipWhenMissingDependencies = 1;
+    };
+};
+
+// Uncomment when needed
+#include "CfgVTOLbase.hpp"
+//#include "CfgMarkers.hpp"
+//#include "CfgWeapons.hpp"
