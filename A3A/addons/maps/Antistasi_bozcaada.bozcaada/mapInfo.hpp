ceb7b4307fd6789d5c8fca0d76aa918505238f90
Xestiny <73776963+Xestiny@users.noreply.github.com>
2026-03-25 04:27:51 -0700

    Add files via upload
    Signed-off-by: Xestiny <73776963+Xestiny@users.noreply.github.com>

---
+++ A3A/addons/maps/Antistasi_bozcaada.bozcaada/mapInfo.hpp
@@ -0,0 +1,32 @@
+class bozcaada {
+	population[] = {
+		{"merkez_city",1200}
+	};
+	disabledTowns[] = {}; //no towns that need to be disabled
+	antennas[] = {
+		{12777.8,12908.4,0.576645},{12836.6,10356.5,0.0119171},{12820.8,8506.88,1.52588e-05},{7671.17,12459.7,-0.000488281},{12671.4,7410.87,0.0109253},{14565.1,11960.3,0.760006}
+	};
+	antennasBlacklistIndex[] = {
+	};
+	banks[] = {
+		{14117,12009.3,17.3781};
+		
+	};
+	garrison[] = {
+		{},{"airport_1","resource_5","outpost_19","outpost_18"},{},{"control","control_1"}
+	};
+	fuelStationTypes[] = {
+		"Land_FuelStation_Feed_F","Land_fs_feed_F","Land_FuelStation_01_pump_malevil_F","Land_FuelStation_01_pump_F","Land_FuelStation_02_pump_F","Land_FuelStation_03_pump_F"
+	};
+	milAdministrations[] = {
+		{14050.9,12273.2,0.27},{14330.126,11873.051,0}
+		
+	};
+	climate = "arid";
+	buildObjects[] = {
+		BUILDABLES_HISTORIC,
+		BUILDABLES_UNIVERSAL,
+		BUILDABLES_MODERN_SAND,
+		BUILDABLES_ARID
+	};
+};
