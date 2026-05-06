4098e0c76fa3cf6df0a3f35e60216740162328ab
Xestiny <73776963+Xestiny@users.noreply.github.com>
2026-05-06 10:29:08 -0700

    Add files via upload
    Signed-off-by: Xestiny <73776963+Xestiny@users.noreply.github.com>

--- A3A/addons/maps/mapInfo.hpp
+++ A3A/addons/maps/mapInfo.hpp
@@ -1,68 +1,64 @@
-class mapInfo {
-    #include "Antistasi_Altis.Altis\mapInfo.hpp"
-    #include "Antistasi_Malden.Malden\mapInfo.hpp"
-    #include "Antistasi_SefrouRamal.SefrouRamal\mapInfo.hpp"
-    #include "Antistasi_Stratis.Stratis\mapInfo.hpp"
-	#include "Antistasi_chernarus.chernarus\mapInfo.hpp"
-	#include "Antistasi_chernarus_summer.chernarus_summer\mapInfo.hpp"
-	#include "Antistasi_chernarus_winter.chernarus_winter\mapInfo.hpp"
-    #include "Antistasi_cup_chernarus_A3.cup_chernarus_A3\mapInfo.hpp"
-    #include "Antistasi_Enoch.Enoch\mapInfo.hpp"
-    #include "Antistasi_Esseker.Esseker\mapInfo.hpp"
-    #include "Antistasi_Napf.Napf\mapInfo.hpp"
-	#include "Antistasi_NapfWinter.NapfWinter\mapInfo.hpp"
-    #include "Antistasi_blud_vidda.blud_vidda\mapInfo.hpp"
-    #include "Antistasi_vt7.vt7\mapInfo.hpp"
-    #include "Antistasi_takistan.takistan\mapInfo.hpp"
-    #include "Antistasi_Tanoa.Tanoa\mapInfo.hpp"
-    #include "Antistasi_cam_lao_nam.cam_lao_nam\mapInfo.hpp"
-    #include "Antistasi_vn_khe_sanh.vn_khe_sanh\mapInfo.hpp"
-    #include "Antistasi_Sehreno.Sehreno\mapInfo.hpp"
-	#include "Antistasi_sara.sara\mapInfo.hpp"
-	#include "Antistasi_Kunduz.Kunduz\mapInfo.hpp"
-	#include "Antistasi_Tembelan.Tembelan\mapInfo.hpp"
-	#include "Antistasi_tem_anizay.tem_anizay\mapInfo.hpp"
-	#include "Antistasi_SPE_Normandy.SPE_Normandy\mapInfo.hpp"
-	#include "Antistasi_lingor3.lingor3\mapInfo.hpp"
-	#include "Antistasi_lythium.lythium\mapInfo.hpp"
-	#include "Antistasi_Panthera3.Panthera3\mapInfo.hpp"
-	#include "Antistasi_Winthera3.Winthera3\mapInfo.hpp"
-	#include "Antistasi_abramia.abramia\mapInfo.hpp"
-	#include "Antistasi_OPTRE_Madrigal.OPTRE_Madrigal\mapInfo.hpp"
-	#include "Antistasi_rhspkl.rhspkl\mapInfo.hpp"
-	#include "Antistasi_Kapaulio.Kapaulio\mapInfo.hpp"
-	#include "Antistasi_Staszow.Staszow\mapInfo.hpp"
-	#include "Antistasi_StaszowWinter.StaszowWinter\mapInfo.hpp"
-	#include "Antistasi_iron_excelsior_Tobruk.iron_excelsior_Tobruk\mapInfo.hpp"
-	#include "Antistasi_ruha.ruha\mapInfo.hpp"
-	#include "Antistasi_gm_weferlingen_winter.gm_weferlingen_winter\mapInfo.hpp"
-    #include "Antistasi_gm_weferlingen_summer.gm_weferlingen_summer\mapInfo.hpp"
-    #include "Antistasi_green_sea.green_sea\mapInfo.hpp"
-	#include "Antistasi_brf_sumava.brf_sumava\mapInfo.hpp"
-	#include "Antistasi_spex_utah_beach.spex_utah_beach\mapInfo.hpp"
-	#include "Antistasi_pja310.pja310\mapInfo.hpp"
-	#include "Antistasi_namalsk.namalsk\mapInfo.hpp"
-	#include "Antistasi_WW2_Omaha_Beach.WW2_Omaha_Beach\mapInfo.hpp"
-	#include "Antistasi_UMB_Colombia.UMB_Colombia\mapInfo.hpp"
-	#include "Antistasi_tem_kujari.tem_kujari\mapInfo.hpp"
-	#include "Antistasi_stozec.stozec\mapInfo.hpp"
-	#include "Antistasi_psyfx_pht.psyfx_pht\mapInfo.hpp"
-	#include "Antistasi_yulakia.yulakia\mapInfo.hpp"
-	#include "Antistasi_Gulfcoast.Gulfcoast\mapInfo.hpp"
-	#include "Antistasi_SPE_Mortain.SPE_Mortain\mapInfo.hpp"
-	#include "Antistasi_tem_chernarus.tem_chernarus\mapInfo.hpp"
-	#include "Antistasi_tem_chernarusw.tem_chernarusw\mapInfo.hpp"
-	#include "Antistasi_mehland.mehland\mapInfo.hpp"
-	#include "Antistasi_pulau.pulau\mapInfo.hpp"
-	#include "Antistasi_fapovo.fapovo\mapInfo.hpp"
-	#include "Antistasi_chernarusredux.chernarusredux\mapInfo.hpp"
-	#include "Antistasi_stubbhult.stubbhult\mapInfo.hpp"
-	#include "Antistasi_kunduz_valley.kunduz_valley\mapInfo.hpp"
-	#include "Antistasi_isladuala3.isladuala3\mapInfo.hpp"
-	#include "Antistasi_VTF_Korsac.VTF_Korsac\mapInfo.hpp"
-	#include "Antistasi_VTF_Korsac_Winter.VTF_Korsac_Winter\mapInfo.hpp"
-	#include "Antistasi_regero.regero\mapInfo.hpp"
-	#include "Antistasi_drakovac.drakovac\mapInfo.hpp"
-	#include "Antistasi_green_sea_2023.green_sea_2023\mapInfo.hpp"
-	#include "Antistasi_bozcaada.bozcaada\mapInfo.hpp"
+#include "\x\A3A\addons\maps\BuildObjectsList.hpp"
+class egl_laghisola {
+	population[] = {
+	{"Tama", 6},
+	{"Itaura", 268},
+	{"Esperan", 259},
+	{"Vala", 56},
+	{"Valado", 16},
+	{"SandeRasna", 25},
+	{"Cacola", 105},
+	{"ManikauaMn", 126},
+	{"Cape", 112},
+	{"Baltarni", 200},
+	{"Santiani", 97},
+	{"Verno", 60},
+	{"Rica", 91},
+	{"Kabypana", 268},
+	{"SandeRopa", 175},
+	{"Fazen", 36},
+	{"Guaytan", 33},
+	{"Binora", 99},
+	{"MonteSore", 104},
+	{"Sollano", 8},
+	{"Zaro", 79},
+	{"Carayan", 164},
+	{"Cubanua", 100},
+	{"Marlan", 91},
+	{"Saray", 116},
+	{"MonteCristoPrison", 141},
+	{"Komerca", 72},
+	{"PalmOilHill", 142},
+	{"Elisio", 154},
+	{"Penedo", 211},
+	{"doldrums", 201},
+	{"Joianopantano", 191}
+
+	};
+	disabledTowns[] = {"Esperan","Valado","Vala","Baltarni","Cubanua","Fazen","Guaytan","Kabypana","Komerca","ManikauaMn","MonteCristoPrison","MonteSore","SandeRasna","Saray","PalmOilHill","Tama","Carayan","Penedo","Marlan"}; //no towns that need to be disabled
+	
+	antennas[] = {
+		{5115.24,4848.42,0.0013237},{4734.13,4095.8,-1.90735e-06},{4823.22,6413.39,0.0394974},{3689.7,3716.6,0.0581551},{7279.1,4381.64,0},{7384.11,3609.24,0.809658},{7536.85,7526.46,0},{2453.32,7271.85,0},{2337.33,7470.55,0.00394249},{5454.77,8781.2,9.15527e-05},{1274.12,8687.63,0},{1366.85,335.166,0.445953}
+	};
+	antennasBlacklistIndex[] = {1,2,4,8,12};
+	banks[] = {
+		{6533.75,2389.875,0}
+	};
+	garrison[] = {
+		{},{airport_2,outpost_8,outpost_7,milbase_3},{},{control_9}
+	};
+	fuelStationTypes[] = {
+		"Land_FuelStation_Feed_F","Land_fs_feed_F","Land_FuelStation_01_pump_malevil_F","Land_FuelStation_01_pump_F","Land_FuelStation_02_pump_F","Land_FuelStation_03_pump_F","Land_A_FuelStation_Feed","Land_Ind_FuelStation_Feed_EP1","Land_FuelStation_Feed_PMC"
+	};
+	milAdministrations[] = {
+		{6809.821,2405.739,4.538},{1806.189,5855.292,3.06},{8404.146,5429.703,15.591}
+		
+	};
+	climate = "Temperate";
+	buildObjects[] = {
+		BUILDABLES_HISTORIC,
+		BUILDABLES_MODERN_GREEN,
+		BUILDABLES_TEMPERATE,
+		BUILDABLES_UNIVERSAL
+	};
 };
