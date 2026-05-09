8ae0770722bf05e2d4310d469ca2b8e72abb8c67
Xestiny <73776963+Xestiny@users.noreply.github.com>
2026-05-09 10:26:45 -0700

    Add files via upload
    Signed-off-by: Xestiny <73776963+Xestiny@users.noreply.github.com>

---
+++ A3A/addons/maps/Antistasi_126map.126map/mapInfo.hpp
@@ -0,0 +1,66 @@
+#include "\x\A3A\addons\maps\BuildObjectsList.hpp"
+class 126map {
+	population[] = {
+{"Fort St-Jean", 344},
+{"Saint-George", 1149},
+{"Gueto", 788},
+{"Kawan", 104},
+{"Centrale electrique", 442},
+{"Usine Sugardaddy", 243},
+{"Tahos", 123},
+{"Pohaku", 157},
+{"Wiki bar", 37},
+{"Saint-Martin", 97},
+{"Bellevue", 179},
+{"Keyao", 96},
+{"Navos", 274},
+{"St-Macaulay", 88},
+{"Nice", 146},
+{"St-Helene", 83},
+{"Jericho", 62},
+{"Favelas", 937},
+{"Bas fort", 366},
+{"Nokeri", 157},
+{"Karawa", 35},
+{"Malda", 96},
+{"Saunom", 437}
+	};
+	disabledTowns[] = {"DefaultKeyPoint5","DefaultKeyPoint7","DefaultKeyPoint18","DefaultKeyPoint12","DefaultKeyPoint51","DefaultKeyPoint8","DefaultKeyPoint49","DefaultKeyPoint3","DefaultKeyPoint13"}; //no towns that need to be disabled
+	antennas[] = {
+{3534.58,3632.94,0},
+{4969.58,5776.16,0},
+{2192.59,2150.28,-3.12082},
+{5834.54,5363.01,0.3862},
+{1251.77,1678.5,-0.153122},
+{5449.43,336.34,-0.0428047},
+{648.097,6337.48,-0.160126},
+{6201.689,4031.196,0},
+{6299.520,875.413,10.637},
+{807.25,4647.5,0},
+{4358.75,1848.5,0}
+
+		
+	};
+	antennasBlacklistIndex[] = {};
+	banks[] = {
+		
+	};
+	garrison[] = {
+		{},{airport_2,outpost_10,outpost_14,outpost_9,seaport_2,outpost_8,outpost_15,resource_8,resource_6,resource_7,outpost_20},{},{control,control_1,control_2,control_3,control_4,control_5}
+	};
+	fuelStationTypes[] = {
+		"Land_FuelStation_Feed_F","Land_fs_feed_F","Land_FuelStation_01_pump_malevil_F","Land_FuelStation_01_pump_F","Land_FuelStation_02_pump_F","Land_FuelStation_03_pump_F"
+	};
+	milAdministrations[] = {
+		{5783.37,4719.288,0},{5673.453,4617.384,0},{3895.574,4322.197,0},{3178.584,5923.52,0},{3247.882,1338.794,0},{7166.666,2407.506,0},{5067.988,2617.851,0},{6034.75,6448,0}
+		
+	};
+	climate = "tropical";
+	buildObjects[] = {
+		BUILDABLES_HISTORIC,
+		BUILDABLES_UNIVERSAL,
+		BUILDABLES_MODERN_GREEN,	
+		BUILDABLES_TROPIC
+	
+	};
+};
