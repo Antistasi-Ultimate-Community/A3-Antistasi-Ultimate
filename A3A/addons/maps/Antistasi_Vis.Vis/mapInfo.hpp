f1bf83b7860cf099936fec4ff3361a3c756ce626
Xestiny <73776963+Xestiny@users.noreply.github.com>
2026-06-02 02:58:14 -0700

    Add files via upload
    Signed-off-by: Xestiny <73776963+Xestiny@users.noreply.github.com>

--- A3A/addons/maps/Antistasi_Vis.Vis/mapInfo.hpp
+++ A3A/addons/maps/Antistasi_Vis.Vis/mapInfo.hpp
@@ -1,3 +1,48 @@
-class mapInfo {
-	#include "Antistasi_Vis.Vis\mapInfo.hpp"
+#include "\x\A3A\addons\maps\BuildObjectsList.hpp"
+class Vis {
+	population[] = {
+		{"Komiza", 403},
+{"Vis", 178},
+{"DefaultKeyPoint4", 173},
+{"DefaultKeyPoint5", 124},
+{"DefaultKeyPoint6", 17},
+{"DefaultKeyPoint7", 55},
+{"DefaultKeyPoint8", 95},
+{"DefaultKeyPoint9", 73},
+{"DefaultKeyPoint10", 103},
+{"DefaultKeyPoint11", 115},
+{"DefaultKeyPoint12", 145},
+{"DefaultKeyPoint13", 48},
+{"DefaultKeyPoint3", 42},
+{"DefaultKeyPoint32", 8}
+		
+	};
+	disabledTowns[] = {}; //no towns that need to be disabled
+	antennas[] = {
+		{12665.875,8375.25,0},{17056.588,16843.475,3.71},{9956.25,13369.75,0},{6316.625,8043.125,1.218},{5875.625,11279,0}
+		
+	};
+	antennasBlacklistIndex[] = {};
+	banks[] = {
+		
+	};
+	garrison[] = {
+		{},{"airport_1","outpost_12","outpost_16","outpost_20","outpost_19","factory_4"},{},{"control","control_1","control_2"}
+	};
+	fuelStationTypes[] = {
+		"Land_FuelStation_Feed_F","Land_fs_feed_F","Land_FuelStation_01_pump_malevil_F","Land_FuelStation_01_pump_F","Land_FuelStation_02_pump_F","Land_FuelStation_03_pump_F"
+	};
+	milAdministrations[] = {
+		{4252.625,10158,0},{9973.375,8353.75,0},{17836.375,11976.125,0}
+		
+	};
+	climate = "temperate";
+	buildObjects[] = {
+		BUILDABLES_HISTORIC,
+		BUILDABLES_MODERN_GREEN,
+		BUILDABLES_TEMPERATE,
+		BUILDABLES_UNIVERSAL,
+		BUILDABLES_CUP
+		
+	};
 };
