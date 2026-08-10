f5770577fd70d7e1838f1ccebe630f1e93318daf
MaxxLite <162753200+UAC-MaxxLite@users.noreply.github.com>
2026-08-10 20:01:32 +0300

    basic functionality
    player info tab and hover functionality

--- A3A/addons/ultimate/CfgFunctions.hpp
+++ A3A/addons/ultimate/CfgFunctions.hpp
@@ -58,6 +58,7 @@ class CfgFunctions
             class markerBrowser {};
             class markerContextMenu {};
             class mrkUpdateBulk {};
+            class playerContextMenu {};
         };
         class REINF
         {
