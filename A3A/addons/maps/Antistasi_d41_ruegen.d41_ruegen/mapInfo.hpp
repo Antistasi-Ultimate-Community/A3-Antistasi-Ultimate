dedb14a53054d0a528a8911d0afb687afcfcd50a
stutpip123 <77023662+stutpip123@users.noreply.github.com>
2026-08-17 21:43:29 +0100

    Ruegen

---
+++ A3A/addons/maps/Antistasi_d41_ruegen.d41_ruegen/mapInfo.hpp
@@ -0,0 +1,80 @@
+#include "..\BuildObjectsList.hpp"
+class d41_ruegen {
+	population[] = {
+{"DefaultKeyPoint1", 514},
+{"DefaultKeyPoint2", 467},
+{"DefaultKeyPoint3", 230},
+{"DefaultKeyPoint4", 236},
+{"DefaultKeyPoint5", 211},
+{"DefaultKeyPoint6", 122},
+{"DefaultKeyPoint7", 78},
+{"DefaultKeyPoint8", 60},
+{"DefaultKeyPoint9", 27},
+{"DefaultKeyPoint12", 264},
+{"DefaultKeyPoint13", 199},
+{"DefaultKeyPoint15", 158},
+{"DefaultKeyPoint16", 68},
+{"DefaultKeyPoint17", 75},
+{"DefaultKeyPoint19", 129},
+{"DefaultKeyPoint21", 122},
+{"DefaultKeyPoint23", 88},
+{"DefaultKeyPoint24", 165},
+{"DefaultKeyPoint25", 58},
+{"DefaultKeyPoint27", 170},
+{"DefaultKeyPoint28", 28},
+{"DefaultKeyPoint29", 124},
+{"DefaultKeyPoint31", 74},
+{"DefaultKeyPoint32", 35},
+{"DefaultKeyPoint33", 67},
+{"DefaultKeyPoint35", 38},
+{"DefaultKeyPoint36", 121},
+{"DefaultKeyPoint38", 39},
+{"DefaultKeyPoint39", 94},
+{"DefaultKeyPoint41", 77},
+{"DefaultKeyPoint42", 192},
+{"DefaultKeyPoint46", 271},
+{"DefaultKeyPoint47", 127},
+{"DefaultKeyPoint49", 21},
+{"DefaultKeyPoint50", 36},
+{"DefaultKeyPoint51", 31},
+{"DefaultKeyPoint52", 24},
+{"DefaultKeyPoint53", 70},
+{"DefaultKeyPoint54", 151},
+{"DefaultKeyPoint58", 104},
+{"DefaultKeyPoint59", 37},
+{"DefaultKeyPoint60", 62},
+{"DefaultKeyPoint61", 118},
+{"DefaultKeyPoint65", 115},
+{"DefaultKeyPoint66", 8},
+{"DefaultKeyPoint67", 109},
+{"DefaultKeyPoint68", 89},
+{"DefaultKeyPoint69", 84},
+{"DefaultKeyPoint71", 66},
+{"DefaultKeyPoint72", 212},
+{"DefaultKeyPoint73", 57},
+{"DefaultKeyPoint84", 48},
+{"DefaultKeyPoint92", 35},
+{"DefaultKeyPoint93", 44}
+	};
+	disabledTowns[] = {};
+	antennas[] = {
+		{572.267,4036.186,0},{2445.549,511.952,0.051},{4333.108,2344.826,0},{7318.033,6047.958,0},{5594.463,5656.618,0.001},{12359.025,5178.446,0.004},{9939.512,8923.543,0},{11460.685,11355.78,0},{6701.408,14445.144,0.003},{1201.445,8269.235,0},{3965.688,10055.528,0}
+	};
+	antennasBlacklistIndex[] = {};
+	banks[] = {}; //no suitable building available
+	garrison[] = {
+		{},{"airport_2","factory","seaport_2","seaport_3","outpost_24","outpost_25","control_5","control_50","control_51","control_52","control_53","control_54","control_55","control_56","control_57"},{},{"control_5","control_50","control_51","control_52","control_53","control_54","control_55","control_56","control_57"}
+	};
+	fuelStationTypes[] = {"Land_A_FuelStation_Feed","Land_Ind_FuelStation_Feed_EP1","Land_FuelStation_Feed_PMC","Land_Fuelstation","Land_Fuelstation_army","Land_Benzina_schnell"};
+	milAdministrations[] = {
+		{7744.044,6785.025,0.270},{3759.166,3130.086,0},{9634.010,9924.245,0.109},{2047.724,2198.299,0}
+	};
+	climate = "temperate";
+	buildObjects[] = {
+		BUILDABLES_HISTORIC,
+		BUILDABLES_UNIVERSAL,
+		BUILDABLES_MODERN_GREEN,
+		BUILDABLES_TEMPERATE,
+		BUILDABLES_CUP
+	};
+};
