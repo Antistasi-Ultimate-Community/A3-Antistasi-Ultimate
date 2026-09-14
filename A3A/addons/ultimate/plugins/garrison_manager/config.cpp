0224c447570c4a62f4fe15eb6dbdac190f103acb
Oliver Schieche <post@oliver-schieche.de>
2026-09-14 15:13:18 +0200

    Integrate into game

--- A3A/addons/ultimate/plugins/garrison_manager/config.cpp
+++ A3A/addons/ultimate/plugins/garrison_manager/config.cpp
@@ -11,3 +11,4 @@ class CfgPatches {
 
 #include "CfgEventHandlers.hpp"
 #include "dialog.hpp"
+#include "Params.hpp"
--- A3A/addons/ultimate/plugins/garrison_manager/script_component.hpp
+++ A3A/addons/ultimate/plugins/garrison_manager/script_component.hpp
@@ -1,5 +1,5 @@
 #define SUBCOMPONENT garrison_manager
 #include "..\plugin.hpp"
 
-#define CBA_EVENT_LOCATION_SELECTED QUOTE(TRIPLES(ADDON,event,locationSelected))
-#define CBA_EVENT_PRESELECT_LOCATION QUOTE(TRIPLES(ADDON,event,preselectLocation))
+#define CBA_EVENT_LOCATION_SELECTED QUOTE(TRIPLES(SUBADDON,event,locationSelected))
+#define CBA_EVENT_PRESELECT_LOCATION QUOTE(TRIPLES(SUBADDON,event,preselectLocation))
